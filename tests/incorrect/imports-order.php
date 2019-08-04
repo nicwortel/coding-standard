ffd12b24c62363233ad0eb860d6ced010b062bb4
Nic Wortel <nic@nicwortel.nl>
2019-08-04 13:48:41 +0200

    Fix the enforced order of imports to conform with PSR-12

---
+++ tests/incorrect/imports-order.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Foo;
+
+use DateTime;
+use const PHP_VERSION;
+use function array_filter;
+
+$now = new DateTime();
+
+$array = array_filter([1, 2, 3]);
+
+$version = PHP_VERSION;
